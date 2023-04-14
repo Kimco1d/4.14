1f2c6b42a51e9dc20590520b14352d62eae6a767
Chan K <cold@Chanui-MacBookAir.local>
2023-04-14 14:13:41 +0900

    first commit

---
+++ backend/routes/user.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const router = express.Router();
+
+const client = new PrismaClient();
+
+router.post("/", async (req, res) => {
+  try {
+    const { account } = req.body;
+
+    const existUser = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "Already exist accout." });
+    }
+
+    const user = await client.user.create({
+      data: {
+        account,
+      },
+    });
+
+    res.json({ ok: true, user });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/:account", async (req, res) => {
+  try {
+    const { account } = req.params;
+
+    const user = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        error: "Not exist user.",
+      });
+    }
+
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
